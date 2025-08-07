64530ef28639a7081b9225cdb2666fffafd3a155
jeetdev1997 <ajeetdubey986@gmail.com>
2025-08-07 23:23:48 +0530

    write a code to find hypotenuse

---
+++ calculate_hypotenuse.py
@@ -0,0 +1,9 @@
+def calculate_hypotenuse(base , height):
+    hypotenuse = ((base ** 2) + (height ** 2)) ** 0.5
+    return hypotenuse
+
+
+hypotenuse = calculate_hypotenuse(5, 7)
+
+print(hypotenuse)
+
